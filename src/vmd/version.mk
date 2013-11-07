62bfd3f4007eff1579c7bf9601f1319c57c3ed16
root <root@gcn-20-88.sdsc.edu>
2013-11-06 22:22:26 -0800

    vmd version.mk

---
+++ src/vmd/version.mk
@@ -0,0 +1,20 @@
+NAME    = vmd
+VERSION = 1.9.1
+RELEASE = 0
+
+SRC_SUBDIR	= vmd
+
+VMD_NAME	= vmd
+VMD_VERSION	= 1.9.1
+
+TACHYON_NAME    = tachyon
+TACHYON_VERSION = 0.99b6
+
+ACTC_NAME    = actc
+ACTC_VERSION = 1.1
+
+FLTK_NAME    = fltk
+FLTK_VERSION = 1.3.2
+
+# List of *.tar.gz packages that are part of THIS package
+TAR_GZ_PKGS = $(VMD_NAME)-$(VMD_VERSION).tar.gz $(TACHYON_NAME)-$(TACHYON_VERSION).tar.gz $(ACTC_NAME)-$(ACTC_VERSION).tar.gz $(FLTK_NAME)-$(FLTK_VERSION).tar.gz
