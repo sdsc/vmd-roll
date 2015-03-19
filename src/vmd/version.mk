47ae47f04362aa4f3630027d33567c47e0517ea5
Jerry Greenberg <jpg@comet-devel.local>
2015-03-19 13:40:37 -0700

    new vmd and fltk version

--- src/vmd/version.mk
+++ src/vmd/version.mk
@@ -1,5 +1,5 @@
 NAME    = vmd
-VERSION = 1.9.1
+VERSION = 1.9.2
 RELEASE = 0
 
 RPM.EXTRAS = "AutoReq: no"
@@ -7,7 +7,7 @@ RPM.EXTRAS = "AutoReq: no"
 SRC_SUBDIR	= vmd
 
 VMD_NAME	= vmd
-VMD_VERSION	= 1.9.1
+VMD_VERSION	= 1.9.2
 
 TACHYON_NAME    = tachyon
 TACHYON_VERSION = 0.99b6
@@ -16,7 +16,7 @@ ACTC_NAME    = actc
 ACTC_VERSION = 1.1
 
 FLTK_NAME    = fltk
-FLTK_VERSION = 1.3.2
+FLTK_VERSION = 1.3.3
 
 # List of *.tar.gz packages that are part of THIS package
-TAR_GZ_PKGS = $(VMD_NAME)-$(VMD_VERSION).tar.gz $(TACHYON_NAME)-$(TACHYON_VERSION).tar.gz $(ACTC_NAME)-$(ACTC_VERSION).tar.gz $(FLTK_NAME)-$(FLTK_VERSION).tar.gz
+TAR_GZ_PKGS = $(VMD_NAME)-$(VMD_VERSION).src.tar.gz $(TACHYON_NAME)-$(TACHYON_VERSION).tar.gz $(ACTC_NAME)-$(ACTC_VERSION).tar.gz $(FLTK_NAME)-$(FLTK_VERSION)-source.tar.gz
