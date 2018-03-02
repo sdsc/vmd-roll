1e30fd4988f9400d61a83b56e64412192e899f32
Jim Hayes <jhayes@sdsc.edu>
2018-03-02 13:33:50 -0800

    Set rpm make vars needed for Rocks v7. Resolves APS-715.

--- src/roll-test/version.mk
+++ src/roll-test/version.mk
@@ -1,6 +1,7 @@
 NAME       = sdsc-vmd-roll-test
 VERSION    = 1
-RELEASE    = 1
+RELEASE    = 2
 PKGROOT    = /root/rolltests
 
-RPM.EXTRAS = AutoReq:No
+RPM.EXTRAS = AutoReq:No\nAutoProv:No
+RPM.FILES  = $(PKGROOT)/vmd.t
